06f45ce10a127241a9e831f56fb2c39768ea951c
aditya Pidikiti <adityapidikiti@adityas-MacBook-Air.local>
2019-07-03 13:05:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/application/Artist.java
@@ -0,0 +1,36 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+    public class Artist {
+        private String artistId;
+        private String artistName;
+        private String artistGenre;
+
+        public Artist(){
+
+        }
+
+        public Artist(String artistId, String artistName, String artistGenre) {
+            this.artistId = artistId;
+            this.artistName = artistName;
+            this.artistGenre = artistGenre;
+        }
+
+        public String getArtistId() {
+            return artistId;
+        }
+
+        public String getArtistName() {
+            return artistName;
+        }
+
+        public String getArtistGenre() {
+            return artistGenre;
+        }
+    }
+
---
+++ app/src/main/java/com/example/application/Track.java
@@ -0,0 +1,24 @@
+package com.example.application;
+public class Track {
+    private String id;
+    private String trackName;
+    private int rating;
+
+    public Track() {
+
+    }
+
+    public Track(String id, String trackName, int rating) {
+        this.trackName = trackName;
+        this.rating = rating;
+        this.id = id;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
